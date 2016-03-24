fadcde60881292efc660e73fea53a2ac0e6dfd27
Saurav <csaurav80@gmail.com>
2016-03-24 19:39:36 +0000

    Task # 4 added a recent scope in the song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,12 @@ class Song < ActiveRecord::Base
   
   validates :name, presence: true, length: { in: 5..50 }
   validates :album_id, :artist_id, presence: true
+
+  # I think requirement should have been to create 
+  # a scope in the album model instead of Song model
+  
+  scope :recent, -> (n) { Album.records(9.months.ago).
+                          limit(n).
+                          order('created_at DESC')
+                        }
 end
